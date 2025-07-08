a3eaec14677f7c62f5f49f37aacb4e50d4059d76
sensen312 <sensen312school@gmail.com>
2025-07-08 02:31:46 -0400

    calendar fix

--- app/(pages)/events/EventsPageClient.tsx
+++ app/(pages)/events/EventsPageClient.tsx
@@ -1,10 +1,6 @@
 "use client";
 import { useTina } from "tinacms/dist/react";
-import {
-  PageQuery,
-  EventConnectionQuery,
-  Event,
-} from "@/tina/__generated__/types";
+import { PageQuery, Event } from "@/tina/__generated__/types";
 import { BlockRenderer } from "@/components/blocks/BlockRenderer";
 
 export default function EventsPageClient(props: {
@@ -13,24 +9,13 @@ export default function EventsPageClient(props: {
     variables: { relativePath: string };
     query: string;
   };
-  eventsData: {
-    data: EventConnectionQuery;
-    variables: Record<string, never>;
-    query: string;
-  };
+  allEvents: Event[];
 }) {
   const { data: page } = useTina(props.pageData);
-  const { data: eventsConnection } = useTina(props.eventsData);
-
-  const allEvents =
-    eventsConnection.eventConnection.edges?.map((edge) => edge.node) || [];
 
   return (
     <main id="main-content">
-      <BlockRenderer
-        blocks={page.page.blocks}
-        allEvents={allEvents as Event[]}
-      />
+      <BlockRenderer blocks={page.page.blocks} allEvents={props.allEvents} />
     </main>
   );
 }
--- app/(pages)/events/page.tsx
+++ app/(pages)/events/page.tsx
@@ -1,16 +1,23 @@
 import client from "@/tina/client";
 import { notFound } from "next/navigation";
 import EventsPageClient from "./EventsPageClient";
+import { Event, PageBlocksEvents_listing } from "@/tina/__generated__/types";
 
 export default async function EventsPage() {
   try {
     const pageResult = await client.queries.page({
       relativePath: "events.mdx",
     });
+    const eventsListingBlock = pageResult.data.page.blocks?.find(
+      (block) => block?.__typename === "PageBlocksEvents_listing"
+    ) as PageBlocksEvents_listing | undefined;
 
-    const eventsResult = await client.queries.eventConnection();
+    const curatedEvents =
+      eventsListingBlock?.events
+        ?.map((item) => item?.event)
+        .filter((event): event is Event => !!event) || [];
 
-    return <EventsPageClient pageData={pageResult} eventsData={eventsResult} />;
+    return <EventsPageClient pageData={pageResult} allEvents={curatedEvents} />;
   } catch (error) {
     console.error("Failed to fetch data for events page:", error);
     notFound();
