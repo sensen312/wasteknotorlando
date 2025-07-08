dbcf6f9b0fdb48ddee1706596a2161873deab253
sensen312 <sensen312school@gmail.com>
2025-07-08 13:59:25 -0400

    next config change

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/wasteknotorlando" : "",
-  assetPrefix: isProd ? "/wasteknotorlando/" : "",
+  assetPrefix: isProd ? "/wasteknotorlando" : "",
   images: {
     unoptimized: true,
   },
