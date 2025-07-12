eda2f0a7a6bb4fdbca53f7e4480ea8c4fd1534b9
sensen312 <sensen312school@gmail.com>
2025-07-12 17:58:57 -0400

    BLOCK FIXES

--- components/blocks/BlockRenderer.tsx
+++ components/blocks/BlockRenderer.tsx
@@ -2,6 +2,7 @@
 import {
   Event,
   PageBlocks,
+  EventAdditional_Blocks,
   PageBlocksEvents_Listing,
 } from "@/tina/__generated__/types";
 import { ImageGalleryBlock } from "./content/ImageGalleryBlock";
@@ -23,8 +24,10 @@ import { TopBannerBlock } from "./TopBannerBlock";
 import { CanvaEmbedBlock } from "./content/CanvaEmbedBlock";
 import type { TinaCMS } from "tinacms";
 
+type AllBlockTypes = PageBlocks | EventAdditional_Blocks;
+
 interface BlockRendererProps {
-  blocks: PageBlocks[] | null | undefined;
+  blocks: AllBlockTypes[] | null | undefined;
   allEvents?: Event[];
   mostUpcomingEvent?: Event | null;
   cms?: TinaCMS;
@@ -51,33 +54,46 @@ export const BlockRenderer = ({
           case "PageBlocksTop_banner":
             return <TopBannerBlock key={i} data={block} />;
           case "PageBlocksSection_header":
+          case "EventAdditional_blocksSection_header":
             return <SectionHeaderBlock key={i} data={block} />;
           case "PageBlocksRich_text_content":
+          case "EventAdditional_blocksRich_text_content":
             return <RichTextContentBlock key={i} data={block} />;
           case "PageBlocksTwo_column":
+          case "EventAdditional_blocksTwo_column":
             return <TwoColumnBlock key={i} data={block} />;
           case "PageBlocksFaq":
+          case "EventAdditional_blocksFaq":
             return <FaqBlock key={i} data={block} />;
           case "PageBlocksImage_gallery":
+          case "EventAdditional_blocksImage_gallery":
             return <ImageGalleryBlock key={i} data={block} />;
           case "PageBlocksButton_group":
+          case "EventAdditional_blocksButton_group":
             return <ButtonGroupBlock key={i} data={block} />;
           case "PageBlocksMission_statement":
+          case "EventAdditional_blocksMission_statement":
             return <MissionStatementBlock key={i} data={block} />;
           case "PageBlocksQuick_links":
+          case "EventAdditional_blocksQuick_links":
             return <QuickLinksBlock key={i} data={block} />;
           case "PageBlocksTeam_board":
+          case "EventAdditional_blocksTeam_board":
             return <TeamBoardBlock key={i} data={block} />;
           case "PageBlocksVolunteer_section":
+          case "EventAdditional_blocksVolunteer_section":
             return <VolunteerBlock key={i} data={block} />;
           case "PageBlocksZeffy_donation":
+          case "EventAdditional_blocksZeffy_donation":
             return <ZeffyDonationBlock key={i} data={block} />;
           case "PageBlocksItem_donation_list":
+          case "EventAdditional_blocksItem_donation_list":
             return <ItemDonationListBlock key={i} data={block} />;
           case "PageBlocksCanva_canvaembed":
+          case "EventAdditional_blocksCanva_canvaembed":
             return <CanvaEmbedBlock key={i} data={block} />;
-
           case "PageBlocksInteractive_calendar":
+          case "EventAdditional_blocksInteractive_calendar":
             return (
               <InteractiveCalendar
                 key={i}
@@ -85,8 +101,8 @@ export const BlockRenderer = ({
                 events={allEvents || []}
               />
             );
-
           case "PageBlocksEvent_spotlight":
+          case "EventAdditional_blocksEvent_spotlight":
             return (
               <EventSpotlightBlock
                 key={i}
@@ -94,8 +110,8 @@ export const BlockRenderer = ({
                 mostUpcomingEvent={mostUpcomingEvent}
               />
             );
-
           case "PageBlocksEvents_listing":
+          case "EventAdditional_blocksEvents_listing":
             const eventListingBlock = block as PageBlocksEvents_Listing;
             const referencedEvents =
               eventListingBlock.events
@@ -110,7 +126,6 @@ export const BlockRenderer = ({
                 isCmsEnabled={isCmsEnabled}
               />
             );
-
           default:
             const templateName = typeNameToLog?.split("_").pop();
             console.warn(
