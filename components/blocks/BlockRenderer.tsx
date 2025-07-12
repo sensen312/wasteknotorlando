ee5847f64efdb3c10c030a04910111eff85b1a69
sensen312 <sensen312school@gmail.com>
2025-07-12 17:27:06 -0400

    block renderer fix

--- components/blocks/BlockRenderer.tsx
+++ components/blocks/BlockRenderer.tsx
@@ -2,23 +2,7 @@
 import {
   Event,
   PageBlocks,
-  PageBlocksTop_banner,
-  PageBlocksSection_header,
-  PageBlocksRich_text_content,
-  PageBlocksTwo_column,
-  PageBlocksFaq,
-  PageBlocksImage_gallery,
-  PageBlocksButtonGroup,
-  PageBlocksInteractive_calendar,
-  PageBlocksMission_statement,
-  PageBlocksQuick_links,
-  PageBlocksEvent_spotlight,
-  PageBlocksTeam_board,
-  PageBlocksVolunteer_section,
-  PageBlocksZeffy_donation,
-  PageBlocksItem_donation_list,
-  PageBlocksEvents_listing,
-  PageBlocksCanva_canvaembed,
+  PageBlocksEvents_Listing,
 } from "@/tina/__generated__/types";
 import { ImageGalleryBlock } from "./content/ImageGalleryBlock";
 import { RichTextContentBlock } from "./content/RichTextContentBlock";
@@ -36,18 +20,11 @@ import { ItemDonationListBlock } from "./sections/ItemDonationListBlock";
 import EventsListing from "../sections/EventsListing";
 import InteractiveCalendar from "../sections/InteractiveCalendar";
 import { TopBannerBlock } from "./TopBannerBlock";
-import { EventDetailsBlock } from "./event/EventDetailsBlock";
-import { EventImageBlock } from "./event/EventImageBlock";
-import { EventDirectionsBlock } from "./event/EventDirectionsBlock";
-import { EventMapEmbedBlock } from "./event/EventMapEmbedBlock";
-import type { TinaCMS } from "tinacms";
 import { CanvaEmbedBlock } from "./content/CanvaEmbedBlock";
-
-type AnyBlock = PageBlocks;
+import type { TinaCMS } from "tinacms";
 
 interface BlockRendererProps {
-  blocks: AnyBlock[] | null | undefined;
-  eventData?: Event;
+  blocks: PageBlocks[] | null | undefined;
   allEvents?: Event[];
   mostUpcomingEvent?: Event | null;
   cms?: TinaCMS;
@@ -56,7 +33,6 @@ interface BlockRendererProps {
 
 export const BlockRenderer = ({
   blocks,
-  eventData,
   allEvents,
   mostUpcomingEvent,
   cms,
@@ -65,138 +41,80 @@ export const BlockRenderer = ({
   return (
     <>
       {blocks?.map((block, i) => {
-        if (!block) return null;
+        if (!block || !block.__typename) {
+          return null;
+        }
 
-        const templateName = block.__typename?.split("_").pop();
+        const typeNameToLog = block.__typename;
 
-        switch (templateName) {
-          case "details":
-            return eventData ? (
-              <EventDetailsBlock key={i} eventData={eventData} />
-            ) : null;
-          case "image":
-            return eventData ? (
-              <EventImageBlock key={i} eventData={eventData} />
-            ) : null;
-          case "directions":
-            return eventData ? (
-              <EventDirectionsBlock key={i} eventData={eventData} />
-            ) : null;
-          case "embed": // for map
-            return eventData ? (
-              <EventMapEmbedBlock key={i} eventData={eventData} />
-            ) : null;
-          case "banner":
+        switch (block.__typename) {
+          case "PageBlocksTop_banner":
+            return <TopBannerBlock key={i} data={block} />;
+          case "PageBlocksSection_header":
+            return <SectionHeaderBlock key={i} data={block} />;
+          case "PageBlocksRich_text_content":
+            return <RichTextContentBlock key={i} data={block} />;
+          case "PageBlocksTwo_column":
+            return <TwoColumnBlock key={i} data={block} />;
+          case "PageBlocksFaq":
+            return <FaqBlock key={i} data={block} />;
+          case "PageBlocksImage_gallery":
+            return <ImageGalleryBlock key={i} data={block} />;
+          case "PageBlocksButton_group":
+            return <ButtonGroupBlock key={i} data={block} />;
+          case "PageBlocksMission_statement":
+            return <MissionStatementBlock key={i} data={block} />;
+          case "PageBlocksQuick_links":
+            return <QuickLinksBlock key={i} data={block} />;
+          case "PageBlocksTeam_board":
+            return <TeamBoardBlock key={i} data={block} />;
+          case "PageBlocksVolunteer_section":
+            return <VolunteerBlock key={i} data={block} />;
+          case "PageBlocksZeffy_donation":
+            return <ZeffyDonationBlock key={i} data={block} />;
+          case "PageBlocksItem_donation_list":
+            return <ItemDonationListBlock key={i} data={block} />;
+          case "PageBlocksCanva_canvaembed":
+            return <CanvaEmbedBlock key={i} data={block} />;
+
+          case "PageBlocksInteractive_calendar":
             return (
-              <TopBannerBlock key={i} data={block as PageBlocksTop_banner} />
-            );
-          case "header":
-            return (
-              <SectionHeaderBlock
-                key={i}
-                data={block as PageBlocksSection_header}
-              />
-            );
-          case "content":
-            return (
-              <RichTextContentBlock
-                key={i}
-                data={block as PageBlocksRich_text_content}
-              />
-            );
-          case "column":
-            return (
-              <TwoColumnBlock key={i} data={block as PageBlocksTwo_column} />
-            );
-          case "Faq":
-            return <FaqBlock key={i} data={block as PageBlocksFaq} />;
-          case "gallery":
-            return (
-              <ImageGalleryBlock
-                key={i}
-                data={block as PageBlocksImage_gallery}
-              />
-            );
-          case "group":
-            return (
-              <ButtonGroupBlock key={i} data={block as PageBlocksButtonGroup} />
-            );
-          case "calendar":
-            return allEvents ? (
               <InteractiveCalendar
                 key={i}
-                data={block as PageBlocksInteractive_calendar}
-                events={allEvents}
-              />
-            ) : null;
-          case "statement":
-            return (
-              <MissionStatementBlock
-                key={i}
-                data={block as PageBlocksMission_statement}
+                data={block}
+                events={allEvents || []}
               />
             );
-          case "links":
-            return (
-              <QuickLinksBlock key={i} data={block as PageBlocksQuick_links} />
-            );
-          case "spotlight":
+
+          case "PageBlocksEvent_spotlight":
             return (
               <EventSpotlightBlock
                 key={i}
-                data={block as PageBlocksEvent_spotlight}
+                data={block}
                 mostUpcomingEvent={mostUpcomingEvent}
               />
             );
-          case "board":
-            return (
-              <TeamBoardBlock key={i} data={block as PageBlocksTeam_board} />
-            );
-          case "section":
-            return (
-              <VolunteerBlock
-                key={i}
-                data={block as PageBlocksVolunteer_section}
-              />
-            );
-          case "donation":
-            return (
-              <ZeffyDonationBlock
-                key={i}
-                data={block as PageBlocksZeffy_donation}
-              />
-            );
-          case "list":
-            return (
-              <ItemDonationListBlock
-                key={i}
-                data={block as PageBlocksItem_donation_list}
-              />
-            );
-          case "listing":
-            const eventListingData = block as PageBlocksEvents_listing;
-            const referencedEvents = eventListingData.events
-              ?.map((e) => e?.event)
-              .filter(Boolean) as Event[] | [];
+
+          case "PageBlocksEvents_listing":
+            const eventListingBlock = block as PageBlocksEvents_Listing;
+            const referencedEvents =
+              eventListingBlock.events
+                ?.map((e) => e?.event)
+                .filter((e): e is Event => !!e) || [];
             return (
               <EventsListing
                 key={i}
-                data={eventListingData}
+                data={eventListingBlock}
                 allEvents={referencedEvents}
                 cms={cms}
                 isCmsEnabled={isCmsEnabled}
               />
             );
-          case "canvaembed":
-            return (
-              <CanvaEmbedBlock
-                key={i}
-                data={block as PageBlocksCanva_canvaembed}
-              />
-            );
+
           default:
+            const templateName = typeNameToLog?.split("_").pop();
             console.warn(
-              `HOW DID U CHOOSE THIS BLOCK IT DOESNT EVEN EXIST: ${block.__typename}, TEMPLATE: ${templateName}`
+              `HOW DID U CHOOSE THIS BLOCK IT DOESNT EVEN EXIST: ${typeNameToLog}, TEMPLATE: ${templateName}`
             );
             return null;
         }
