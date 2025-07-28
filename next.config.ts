c7d3822c41ba630d0d915d9cad76c8d0b5f33956
sensen312 <sensen312school@gmail.com>
2025-07-28 02:27:23 -0400

    added a admin shortcut

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path(.+)/admin",
+        destination: "/admin/index.html#/~/:path",
+        permanent: false,
+      },
+    ];
+  },
   images: {
     unoptimized: true,
   },
